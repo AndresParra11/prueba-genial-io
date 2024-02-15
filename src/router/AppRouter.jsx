3e15ede23792362a3dda286218fc7b4c6f5ff073
Andrés Parra <pipe.parrab@gmail.com>
2024-02-15 01:24:46 -0500

    Corrigiendo hashRouter.

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,18 +1,18 @@
-import { BrowserRouter, Routes, HashRouter as Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home/Home";
 import Layout from "../components/Layout/Layout";
 import CardDetail from "../pages/CardDetail/CardDetail";
 
 const AppRouter = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="/card/:id" element={<CardDetail />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
